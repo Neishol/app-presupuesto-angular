e7e232d217000350331dc55db08ae3f3bba4caac
Shoce-Ubuntu <shoce.aguero@gmail.com>
2024-11-14 21:12:14 +0100

    Separacion componentes html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceroComponent } from "./cabecero/cabecero.component";
+import { FormularioComponent } from "./formulario/formulario.component";
+import { IngresosComponent } from "./ingresos/ingresos.component";
+import { EgresosComponent } from "./egresos/egresos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceroComponent, FormularioComponent, IngresosComponent, EgresosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
